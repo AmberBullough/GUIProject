d9c24f43c0a32ae4dc7fe4c422e2d07cad5845cc
Amber Bullough <amber@bertbullough.com>
2017-11-13 09:13:25 -0700

    Action listeners

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -51,7 +51,13 @@ public class GUIPanel extends JPanel
 	 */
 	private void setupListeners()
 	{
-		
+		firstButton.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent click)
+					{
+						
+					}
+				});
 	}
 	
 	
