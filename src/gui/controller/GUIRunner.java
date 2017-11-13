dfbaec702078583302ea963270ea009ebb9f985b
Amber Bullough <amber@bertbullough.com>
2017-11-13 07:47:31 -0700

    Added in runenr and code

---
+++ src/gui/controller/GUIRunner.java
@@ -0,0 +1,12 @@
+package gui.controller;
+
+import gui.controller.GUIAppController;
+
+public class GUIRunner 
+{
+	public static void main(String [] args)
+	{
+		GUIAppController myApp = new GUIAppController();
+		myApp.start();
+	}
+}
