d5d12fc7f50979e53ebe1aed0fd13ee402046dc9
Amber Bullough <amber@bertbullough.com>
2017-11-13 08:10:45 -0700

    Added in the app controller

--- src/gui/controller/GUIAppController.java
+++ src/gui/controller/GUIAppController.java
@@ -1,10 +1,17 @@
 package gui.controller;
 
-
+import gui.view.GUIFrame;
 
 public class GUIAppController 
 {
 
+	public GUIFrame appFrame; 
+	
+	public GUIAppController()
+	{
+		this.appFrame = new GUIFrame(this);
+	}
+	
 		public void start() 
 		{
 			
